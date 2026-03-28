fdf9cf97ab0a031f8c795d0e362f7c46fcc22211
rafiramdani321 <rafiramdani321@gmail.com>
2026-03-28 11:43:17 +0700

    feat: add unit test for verifyEmail

--- src/test/java/com/mraffi/ecommerce_api/service/AuthServiceTest.java
+++ src/test/java/com/mraffi/ecommerce_api/service/AuthServiceTest.java
@@ -1,6 +1,8 @@
 package com.mraffi.ecommerce_api.service;
 
 import com.mraffi.ecommerce_api.constant.RoleName;
+import com.mraffi.ecommerce_api.constant.TokenStatus;
+import com.mraffi.ecommerce_api.constant.TokenType;
 import com.mraffi.ecommerce_api.dto.request.auth.RegisterRequest;
 import com.mraffi.ecommerce_api.dto.response.auth.RegisterResponse;
 import com.mraffi.ecommerce_api.entity.Role;
@@ -10,6 +12,8 @@ import com.mraffi.ecommerce_api.exception.ApiException;
 import com.mraffi.ecommerce_api.repository.RoleRepository;
 import com.mraffi.ecommerce_api.repository.TokenRepository;
 import com.mraffi.ecommerce_api.repository.UserRepository;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.SignatureException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,6 +23,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.Instant;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -47,37 +52,63 @@ class AuthServiceTest {
    @Mock
    private PasswordEncoder passwordEncoder;
 
+   @Mock
+   private Claims claims;
+   @Mock
+   private ExpiredJwtException expiredJwtException;
+   @Mock
+   private UnsupportedJwtException unsupportedJwtException;
+   @Mock
+   private MalformedJwtException malformedJwtException;
+   @Mock
+   private SignatureException signatureException;
+
    @InjectMocks
    private AuthServiceImpl authService;
 
-   private RegisterRequest request;
+   private RegisterRequest registerRequest;
+
+   private Token token;
    private Role role;
+   private User user;
 
    @BeforeEach
    void setUp(){
-      request = RegisterRequest.builder()
-              .username("testuser")
-              .fullname("Test User")
-              .email("test@example.com")
+      registerRequest = RegisterRequest.builder()
+              .username("newuser")
+              .fullname("New User")
+              .email("new@example.com")
               .password("123@Password456")
               .confirmPassword("123@Password456")
               .build();
 
+      setField(authService, "emailVerificationTokenExpiration", 900000L);
+
       role = Role.create("CUSTOMER");
 
-      setField(
-              authService,
-              "emailVerificationTokenExpiration",
-              900000L
+      user = User.createLocalUser(
+              "testuser",
+              "Test User",
+              "testuser@example.com",
+              "hashedPassword",
+              role
+      );
+      setField(user, "id", "UUID-USER-TEST");
+
+      token = Token.create(
+              "valid.jwt.token",
+              user,
+              TokenType.EMAIL_ACTIVATION,
+              Instant.now().plusMillis(900000L)
       );
    }
 
    @Test
    void register_success(){
-      when(userRepository.existsByUsername(request.getUsername())).thenReturn(false);
-      when(userRepository.existsByEmail(request.getEmail())).thenReturn(false);
+      when(userRepository.existsByUsername(registerRequest.getUsername())).thenReturn(false);
+      when(userRepository.existsByEmail(registerRequest.getEmail())).thenReturn(false);
       when(roleRepository.findByName(RoleName.CUSTOMER.name())).thenReturn(Optional.of(role));
-      when(passwordEncoder.encode(request.getPassword())).thenReturn("hashedPassword");
+      when(passwordEncoder.encode(registerRequest.getPassword())).thenReturn("hashedPassword");
 
       when(userRepository.save(any(User.class))).thenAnswer(
               invocation -> {
@@ -89,24 +120,24 @@ class AuthServiceTest {
 
       when(jwtService.generateEmailVerificationToken(any())).thenReturn("mockToken");
 
-      RegisterResponse response = authService.register(request);
+      RegisterResponse response = authService.register(registerRequest);
 
       assertNotNull(response);
-      assertEquals("testuser", response.getUsername());
-      assertEquals("Test User", response.getFullname());
-      assertEquals("test@example.com", response.getEmail());
+      assertEquals(registerRequest.getUsername(), response.getUsername());
+      assertEquals(registerRequest.getFullname(), response.getFullname());
+      assertEquals(registerRequest.getEmail(), response.getEmail());
       assertFalse(response.getIsVerified());
 
       verify(userRepository).save(any(User.class));
       verify(tokenRepository).save(any(Token.class));
-      verify(emailService).sendVerificationEmail("test@example.com", "mockToken");
+      verify(emailService).sendVerificationEmail(response.getEmail(), "mockToken");
    }
 
    @Test
    void register_usernameAlreadyExists(){
-      when(userRepository.existsByUsername(request.getUsername())).thenReturn(true);
+      when(userRepository.existsByUsername(registerRequest.getUsername())).thenReturn(true);
 
-      ApiException ex = assertThrows(ApiException.class, () -> authService.register(request));
+      ApiException ex = assertThrows(ApiException.class, () -> authService.register(registerRequest));
 
       assertNotNull(ex.getErrors());
 
@@ -120,10 +151,10 @@ class AuthServiceTest {
 
    @Test
    void register_emailAlreadyExists(){
-      when(userRepository.existsByUsername(request.getUsername())).thenReturn(false);
-      when(userRepository.existsByEmail(request.getEmail())).thenReturn(true);
+      when(userRepository.existsByUsername(registerRequest.getUsername())).thenReturn(false);
+      when(userRepository.existsByEmail(registerRequest.getEmail())).thenReturn(true);
 
-      ApiException ex = assertThrows(ApiException.class, () -> authService.register(request));
+      ApiException ex = assertThrows(ApiException.class, () -> authService.register(registerRequest));
 
       assertNotNull(ex.getErrors());
 
@@ -137,11 +168,11 @@ class AuthServiceTest {
 
    @Test
    void register_passwordMismatch(){
-      request.setConfirmPassword("wrongConfirmPassword");
-      when(userRepository.existsByUsername(request.getUsername())).thenReturn(false);
-      when(userRepository.existsByEmail(request.getEmail())).thenReturn(false);
+      registerRequest.setConfirmPassword("wrongConfirmPassword");
+      when(userRepository.existsByUsername(registerRequest.getUsername())).thenReturn(false);
+      when(userRepository.existsByEmail(registerRequest.getEmail())).thenReturn(false);
 
-      ApiException ex = assertThrows(ApiException.class, () -> authService.register(request));
+      ApiException ex = assertThrows(ApiException.class, () -> authService.register(registerRequest));
 
       assertNotNull(ex.getErrors());
 
@@ -155,11 +186,11 @@ class AuthServiceTest {
 
    @Test
    void register_roleNotFound(){
-      when(userRepository.existsByUsername(request.getUsername())).thenReturn(false);
-      when(userRepository.existsByEmail(request.getEmail())).thenReturn(false);
+      when(userRepository.existsByUsername(registerRequest.getUsername())).thenReturn(false);
+      when(userRepository.existsByEmail(registerRequest.getEmail())).thenReturn(false);
       when(roleRepository.findByName(RoleName.CUSTOMER.name())).thenReturn(Optional.empty());
 
-      ApiException ex = assertThrows(ApiException.class, () -> authService.register(request));
+      ApiException ex = assertThrows(ApiException.class, () -> authService.register(registerRequest));
 
       assertNotNull(ex.getErrors());
 
@@ -172,4 +203,187 @@ class AuthServiceTest {
       verify(userRepository, never()).save(any());
    }
 
+   @Test
+   void verifyEmail_success(){
+      when(tokenRepository.findByToken(token.getToken())).thenReturn(Optional.of(token));
+
+      String mockUserId = user.getId();
+      when(claims.getSubject()).thenReturn(mockUserId);
+      setField(user, "id", mockUserId);
+
+      when(jwtService.validateToken(token.getToken())).thenReturn(claims);
+      when(userRepository.findById(mockUserId)).thenReturn(Optional.of(user));
+
+      authService.verifyEmail(token.getToken());
+
+      assertTrue(user.getIsVerified());
+      assertEquals(TokenStatus.USED, token.getTokenStatus());
+
+      verify(userRepository, times(1)).save(user);
+      verify(tokenRepository, times(1)).save(token);
+   }
+
+   @Test
+   void verifyEmail_failed_tokenNotFound(){
+      when(tokenRepository.findByToken("invalid-token")).thenReturn(Optional.empty());
+
+      ApiException ex = assertThrows(ApiException.class, () -> authService.verifyEmail("invalid-token"));
+
+      assertNotNull(ex.getErrors());
+
+      assertEquals("TOKEN_NOT_FOUND", ex.getCode());
+      assertEquals(HttpStatus.NOT_FOUND, ex.getStatus());
+
+      assertTrue(ex.getErrors().containsKey("token"));
+      assertTrue(ex.getErrors().get("token").contains("Invalid token"));
+
+      verify(userRepository, never()).save(any());
+      verify(tokenRepository, never()).save(any());
+   }
+
+   @Test
+   void verifyEmail_failed_tokenAlreadyUsed(){
+      when(tokenRepository.findByToken(token.getToken())).thenReturn(Optional.of(token));
+      token.setTokenStatus(TokenStatus.USED);
+
+      ApiException ex = assertThrows(ApiException.class, () -> authService.verifyEmail(token.getToken()));
+
+      assertNotNull(ex.getErrors());
+
+      assertEquals("TOKEN_ALREADY_USED", ex.getCode());
+      assertEquals(HttpStatus.BAD_REQUEST, ex.getStatus());
+
+      assertTrue(ex.getErrors().containsKey("token"));
+      assertTrue(ex.getErrors().get("token").contains("Token already used or invalid"));
+
+      verify(userRepository, never()).save(any());
+      verify(tokenRepository, never()).save(any());
+   }
+
+   @Test
+   void verifyEmail_failed_expiredDb(){
+      when(tokenRepository.findByToken(token.getToken())).thenReturn(Optional.of(token));
+      setField(token, "expiredAt", Instant.now().minusSeconds(10));
+
+      ApiException ex = assertThrows(ApiException.class, () -> authService.verifyEmail(token.getToken()));
+
+      assertNotNull(ex.getErrors());
+
+      assertEquals("TOKEN_EXPIRED", ex.getCode());
+      assertEquals(HttpStatus.BAD_REQUEST, ex.getStatus());
+
+      assertTrue(ex.getErrors().containsKey("token"));
+      assertTrue(ex.getErrors().get("token").contains("Token has expired"));
+
+      verify(userRepository, never()).save(any());
+      verify(tokenRepository, never()).save(any());
+   }
+
+   @Test
+   void verifyEmail_failed_jwtExpired(){
+      when(tokenRepository.findByToken(token.getToken())).thenReturn(Optional.of(token));
+
+      when(jwtService.validateToken(token.getToken()))
+              .thenThrow(ExpiredJwtException.class);
+
+      ApiException ex = assertThrows(ApiException.class, () -> authService.verifyEmail(token.getToken()));
+
+      assertNotNull(ex.getErrors());
+
+      assertEquals("TOKEN_EXPIRED", ex.getCode());
+      assertEquals(HttpStatus.BAD_REQUEST, ex.getStatus());
+
+      assertTrue(ex.getErrors().containsKey("token"));
+      assertTrue(ex.getErrors().get("token").contains("Token has expired"));
+
+      verify(userRepository, never()).save(any());
+      verify(tokenRepository, never()).save(any());
+   }
+
+   @Test
+   void verifyEmail_failed_jwtUnsupported(){
+      when(tokenRepository.findByToken(token.getToken())).thenReturn(Optional.of(token));
+
+      when(jwtService.validateToken(token.getToken()))
+              .thenThrow(UnsupportedJwtException.class);
+
+      ApiException ex = assertThrows(ApiException.class, () -> authService.verifyEmail(token.getToken()));
+
+      assertNotNull(ex.getErrors());
+
+      assertEquals("INVALID_TOKEN", ex.getCode());
+      assertEquals(HttpStatus.BAD_REQUEST, ex.getStatus());
+
+      assertTrue(ex.getErrors().containsKey("token"));
+      assertTrue(ex.getErrors().get("token").contains("Invalid token format"));
+
+      verify(userRepository, never()).save(any());
+      verify(tokenRepository, never()).save(any());
+   }
+
+   @Test
+   void verifyEmail_failed_jwtMalformed(){
+      when(tokenRepository.findByToken(token.getToken())).thenReturn(Optional.of(token));
+
+      when(jwtService.validateToken(token.getToken()))
+              .thenThrow(MalformedJwtException.class);
+
+      ApiException ex = assertThrows(ApiException.class, () -> authService.verifyEmail(token.getToken()));
+
+      assertNotNull(ex.getErrors());
+
+      assertEquals("INVALID_TOKEN", ex.getCode());
+      assertEquals(HttpStatus.BAD_REQUEST, ex.getStatus());
+
+      assertTrue(ex.getErrors().containsKey("token"));
+      assertTrue(ex.getErrors().get("token").contains("Invalid token format"));
+
+      verify(userRepository, never()).save(any());
+      verify(tokenRepository, never()).save(any());
+   }
+
+   @Test
+   void verifyEmail_failed_jwtInvalidSignature(){
+      when(tokenRepository.findByToken(token.getToken())).thenReturn(Optional.of(token));
+
+      when(jwtService.validateToken(token.getToken()))
+              .thenThrow(SignatureException.class);
+
+      ApiException ex = assertThrows(ApiException.class, () -> authService.verifyEmail(token.getToken()));
+
+      assertNotNull(ex.getErrors());
+
+      assertEquals("INVALID_SIGNATURE", ex.getCode());
+      assertEquals(HttpStatus.BAD_REQUEST, ex.getStatus());
+
+      assertTrue(ex.getErrors().containsKey("token"));
+      assertTrue(ex.getErrors().get("token").contains("Invalid token signature"));
+
+      verify(userRepository, never()).save(any());
+      verify(tokenRepository, never()).save(any());
+   }
+
+   @Test
+   void verifyEmail_failed_userNotFound(){
+      when(tokenRepository.findByToken(token.getToken())).thenReturn(Optional.of(token));
+      when(jwtService.validateToken(token.getToken())).thenReturn(claims);
+
+      String userIdFromClaims = "unknown-id";
+      when(claims.getSubject()).thenReturn(userIdFromClaims);
+
+      when(userRepository.findById(userIdFromClaims)).thenReturn(Optional.empty());
+
+      ApiException ex = assertThrows(ApiException.class, () -> authService.verifyEmail(token.getToken()));
+
+      assertNotNull(ex.getErrors());
+
+      assertEquals("USER_NOT_FOUND", ex.getCode());
+      assertEquals(HttpStatus.NOT_FOUND, ex.getStatus());
+
+      assertTrue(ex.getErrors().containsKey("global"));
+      assertTrue(ex.getErrors().get("global").contains("User not found"));
+
+      verify(userRepository, never()).save(any());
+      verify(tokenRepository, never()).save(any());
+   }
 }
